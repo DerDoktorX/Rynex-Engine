9cadec75da3cf77127b680913e26ec1e7088e4c0
DerDoktorX <>
2026-08-03 12:19:57 +0200

    Clean code dead code ImGuiLayer.cpp

--- Rynex/src/Rynex/ImGui/ImGuiLayer.cpp
+++ Rynex/src/Rynex/ImGui/ImGuiLayer.cpp
@@ -38,19 +38,15 @@ namespace Rynex {
         ImPlot::CreateContext();
         ImGuiIO& io = ImGui::GetIO();
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
         io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
        
-        //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
-
          float fontSize = 16.0f * 1.0f;
          fontSize = 15.0;
          io.Fonts->AddFontFromFileTTF("../Rynex-Editor/Resources/fonts/Open_Sans/static/OpenSans-Bold.ttf", fontSize);
          io.FontDefault=io.Fonts->AddFontFromFileTTF("../Rynex-Editor/Resources/fonts/Open_Sans/static/OpenSans-Bold.ttf", fontSize);
         
 		ImGui::StyleColorsDark();
-        //ImGui::StyleColorsClassic();
         ImGuiStyle& style = ImGui::GetStyle();
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
@@ -85,26 +81,6 @@ namespace Rynex {
             e.Handled |= e.IsInCategory(EventCategoryKeyBord) & io.WantCaptureKeyboard;
         }
     }
-
-	// void ImGuiLayer::OnUpdate()
-	// {
-    //     ImGuiIO& io = ImGui::GetIO();
-    //     Application& app = Application::Get();
-    //     io.DisplaySize = ImVec2(app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
-    // 
-    //     float time = (float)glfwGetTime();
-    //     io.DeltaTime = m_Time > 0.0f ? (time - m_Time) : (1.0f /60.0f) ;
-    //     m_Time = time;
-    // 
-    //     ImGui_ImplOpenGL3_NewFrame();
-    //     ImGui::NewFrame();
-    // 
-    //     static bool show = true;
-    //     ImGui::ShowDemoWindow(&show);
-    // 
-    //     ImGui::Render();
-    //     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	// }
 
     void ImGuiLayer::Begin()
     {
@@ -179,10 +155,6 @@ namespace Rynex {
         ImGui::ShowDemoWindow(&showGui);
 #endif // RY_IMGUI_DEMO_WINDOW
 
-#if RY_IMPLOT_DEMO_WINDOW
-        static bool showPlot = true;
-        ImPlot::ShowDemoWindow(&showPlot);
-#endif // RY_IMGUI_DEMO_WINDOW
     }
 
 	
