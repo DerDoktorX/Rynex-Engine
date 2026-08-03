fa0187fa7aaed5b5e05a5b379a862fbade3c06c8
DerDoktorX <>
2026-08-03 14:56:35 +0200

    Clean code dead code MemoryAloc.h

--- Rynex/src/Rynex/Core/MemoryAloc.h
+++ Rynex/src/Rynex/Core/MemoryAloc.h
@@ -16,40 +16,6 @@ namespace Rynex {
 				return memoryPtr;
 			}
 
-
-#if 0
-			Allocater CopyHeapMemoryOneToOne();
-			void MoveHeapMemoryOneToOne(Allocater< T, _SizeType >& target);
-			void SwapOneToOne(Allocater<T, _SizeType>& a);
-
-			T GetCopy(_SizeType index) const;
-			T& GetRef(_SizeType index);
-			const T& GetCopyRef(_SizeType index) const;
-
-			template<typename _As_Type>
-			_As_Type GetCopyAs(_SizeType index) const;
-			
-			T& SetValue(_SizeType index, T value);
-			T& SetRef(_SizeType index, T& value);
-			T& SetCopyRef(_SizeType index, const T& value);
-			T& SetMove(_SizeType index, T&& value);
-			
-			T* FirstDataPtr();
-			const T* FirstProtectedDataPtr();
-			T *const FirstDataProtectedPtr();
-			const T *const FirstProtectedDataAndPtr() const;
-			
-			T* LastDataPtr();
-			const T* LastProtectedDataPtr();
-			T *const LastDataProtectedPtr();
-			const T *const LastProtectedDataAndPtr() const;
-
-			T* EndDataPtr();
-			const T* EndProtectedDataPtr();
-			T *const EndDataProtectedPtr();
-			const T *const EndProtectedDataAndPtr() const;
-#endif
-
 		};
 
 		
