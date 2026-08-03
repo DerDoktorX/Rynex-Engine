c573ddbd1cc02ecb035bd5215677bc3f4d361ba4
DerDoktorX <>
2026-08-03 15:13:05 +0200

    Clean code dead code ModelImporter.h

--- Rynex/src/Rynex/Asset/Import/ModelImporter.h
+++ Rynex/src/Rynex/Asset/Import/ModelImporter.h
@@ -9,20 +9,8 @@ namespace Rynex {
 	class ModelImporter
 	{
 	public:
-		
-#if RY_MESH_LOADING
-
 		static Ref<MeshSource> ImportModel(AssetHandle handle, const AssetMetadata& metadata);
 		static Ref<MeshSource> LoadModel(const std::filesystem::path& path);
-
-#else
-		static Ref<Model> ImportModel(AssetHandle handle, const AssetMetadata& metadata);
-		static Ref<Model> LoadModel(const std::filesystem::path& path);
-#endif
-#if RY_EDITOR_ASSETMANGER_THREADE ? 0 : 0
-		static Ref<Model> ImportModelAsync(AssetHandle handle, const AssetMetadata& metadata);
-		static Ref<Model> LoadModelAsync(const std::filesystem::path& path);
-#endif
 
 		static bool ReLoadeModel(AssetHandle handle, const std::filesystem::path& path);
 	};
