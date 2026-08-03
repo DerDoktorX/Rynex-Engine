393a3e48978877396e7cce96c67fc35ee48773de
DerDoktorX <>
2026-08-03 12:04:47 +0200

    Clean code dead code EnumString.cpp

--- Rynex/src/Rynex/Utils/EnumString.cpp
+++ Rynex/src/Rynex/Utils/EnumString.cpp
@@ -5,20 +5,4 @@
 
 namespace Rynex {
 
-	 // template<typename T>
-	 // T EnumString::GetEnumFormString(std::string_view e)
-	 // {
-	 // 	std::optional<T> type = magic_enum::enum_cast<T>(e, magic_enum::case_insensitive);
-	 // 	if (type.has_value())
-	 // 		return type.value();
-	 // 	return T;
-	 // 	// return T::None;
-	 // }
-
-	 // template<typename T>
-	 // std::string_view EnumString::GetStringFromEnum(T e)
-	 // {
-	 // 	// return std::string_view();
-	 // 	return magic_enum::enum_name(e);
-	 // }
 }
