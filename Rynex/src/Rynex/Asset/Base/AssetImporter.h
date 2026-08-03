8ec1bdf273759ce6879efda93666435a86a8c09e
DerDoktorX <>
2026-08-03 15:05:57 +0200

    Clean code dead code AssetImporter.h

--- Rynex/src/Rynex/Asset/Base/AssetImporter.h
+++ Rynex/src/Rynex/Asset/Base/AssetImporter.h
@@ -8,9 +8,6 @@ namespace Rynex {
 	{
 	public: 
 		static Ref<Asset> ImportAsset(AssetHandle handle, const AssetMetadata& metadata);
-#if RY_EDITOR_ASSETMANGER_THREADE ? 0 : 0
-		static Ref<Asset> ImportAssetAsync(AssetHandle handle, const AssetMetadata& metadata);
-#endif
 		static bool ReLoadeAsset(AssetHandle handle, const AssetMetadata& metadata);
 
 		static Ref<Asset> ImportFromAssetPack();
