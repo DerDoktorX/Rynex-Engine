ee42bc258a402962d654e6ef079166d693d51ed3
DerDoktorX <>
2026-08-03 11:08:30 +0200

    Clean code dead code MSDFData.h

--- Rynex/src/Rynex/Renderer/Text/MSDFData.h
+++ Rynex/src/Rynex/Renderer/Text/MSDFData.h
@@ -7,13 +7,8 @@
 namespace Rynex {
 	struct MSDFData
 	{
-#if RY_ENABLE_FONT
 		std::vector<msdf_atlas::GlyphGeometry> Glyphs;
 		msdf_atlas::FontGeometry FontGeometry;
-#else
-		void* Glyphs;
-		void* FontGeometry;
-#endif
 	};
 
 }
