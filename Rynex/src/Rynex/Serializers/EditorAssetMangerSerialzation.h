9449d81ca92a08f9dbedf36e13bf567d77b1f565
DerDoktorX <>
2026-08-03 11:58:56 +0200

    Clean code dead code EditorAssetMangerSerialzation.h

--- Rynex/src/Rynex/Serializers/EditorAssetMangerSerialzation.h
+++ Rynex/src/Rynex/Serializers/EditorAssetMangerSerialzation.h
@@ -17,12 +17,7 @@ namespace Rynex {
 		static bool Serilze(const std::filesystem::path& filepath, AssetRegistry& assetRegistry);
 		static bool Deserilze(const std::filesystem::path& filepath, AssetRegistry& assetRegistry);
 
-#if RY_EDITOR_ASSETMANGER_THREADE
-#if 1
 		static bool SerilzeThread(const std::filesystem::path& filepath, std::map<AssetHandle, AssetMetadata>* handleReg);
 		static bool DeserilzeThread(const std::filesystem::path& filepath, std::map<AssetHandle, AssetMetadata>* handleReg, std::map<std::filesystem::path, AssetHandle>* pathReg);
-#endif
-#endif
-	
 	};
 }
