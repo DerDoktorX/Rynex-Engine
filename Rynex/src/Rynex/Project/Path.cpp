a3cd7a3102e1a2c3807629a85968d09e4beb71d1
DerDoktorX <>
2026-08-07 21:34:51 +0200

    Path add methodes to Path class

--- Rynex/src/Rynex/Project/Path.cpp
+++ Rynex/src/Rynex/Project/Path.cpp
@@ -1,25 +1,127 @@
 #include "rypch.h"
 #include "Path.h"
+#include <Rynex/Project/Project.h>
 
 namespace Rynex {
 
 	Path::Path(const Path& path)
+		: m_Path(path.m_Path)
 	{
 	}
 
 	Path::Path(const std::string& path)
+		: m_Path(path)
 	{
+		ConvertUniverselPath();
 	}
 
 	Path::Path(const std::filesystem::path& path)
+		: m_Path(path)
 	{
+		ConvertUniverselPath();
 	}
 
 	Path::Path(const char* path)
+		: m_Path(path)
 	{
+		ConvertUniverselPath();
 	}
 
 	Path::~Path()
 	{
+	}
+
+	bool Path::IsMarker() const
+	{
+		return Origne::None != GetMarkerOrigine();
+	}
+
+	bool Path::IsRealtive() const
+	{
+		return m_Path.is_relative();
+	}
+
+	bool Path::IsAbsoulte() const
+	{
+		return m_Path.is_absolute();
+	}
+
+	bool Path::IsExisting() const
+	{
+		return std::filesystem::exists(m_Path);
+	}
+
+	Path::Origne Path::GetMarkerOrigine() const
+	{
+		constexpr uint32_t markerCount = 4;
+		constexpr uint32_t markerVaildCount = 2;
+		constexpr const char* markersArray[markerCount] = {
+			RY_PATH_PROJECT_MARKER_STR,
+			RY_PATH_ENGINE_MARKER_STR,
+			RY_PATH_NO_VAILD_MARKER_STR,
+			""
+		};
+
+		uint32_t i = 0;
+		while (i < markerCount && !Project::HasStringInPath(m_Path, markersArray[i]))
+		{
+			i++;
+		}
+		if (markerVaildCount < i)
+		{
+			RY_CORE_ERROR("Diden't found a vaild Marker! {}", i);
+			Origne origne = Origne::None;
+			return origne;
+		}
+		Origne origne = static_cast<Origne>(i);
+		return origne;
+	}
+
+	Path::Origne Path::GetMarkerExpextedPath() const
+	{
+		Origne origne = GetMarkerOrigine();
+		if (Origne::None != origne) {
+			return origne;
+		}
+		RY_CORE_ASSERT(false, "not implement, marker set!");
+
+		return origne;
+	}
+
+
+	void Path::SetMarker(Origne origne)
+	{
+		if (IsMarker()) {
+			ClearOringenMarker();
+		}
+		constexpr const char* markersArray[] = {
+			RY_PATH_NO_VAILD_MARKER_STR
+			RY_PATH_PROJECT_MARKER_STR,
+			RY_PATH_ENGINE_MARKER_STR,
+		};
+		uint32_t index = static_cast<uint32_t>(origne);
+
+		std::string pathStr = m_Path.string() + markersArray[index];
+		m_Path = pathStr;
+	}
+
+	void Path::GenertaMarker()
+	{
+		RY_CORE_ASSERT(false, "not implement!");
+	}
+
+	void Path::ClearOringenMarker()
+	{
+		Origne origne = GetMarkerOrigine();
+		if (Origne::None != origne)
+			return;
+
+		RY_CORE_ASSERT(false, "not implement, marker set!");
+	}
+
+	void Path::ConvertUniverselPath()
+	{
+		std::string genaricPathStr = m_Path.generic_string();
+		m_Path = genaricPathStr;
 	}
 }
--- Rynex/src/Rynex/Project/Path.h
+++ Rynex/src/Rynex/Project/Path.h
@@ -4,21 +4,120 @@ namespace Rynex {
 	class Path
 	{
 	public:
+		enum class Origne
+		{
+			None = 0,
+			Unknown = 1,
+			Engine = 2,
+			Project = 3,
+
+		};
+		// --- public methodes -------------------------------------------------------------------------------------------------------
+
 		Path(const Path& file);
 		Path(const std::string& path);
 		Path(const std::filesystem::path& path);
-		Path(const char* path);
+		Path(const char* path);		~Path();
 
-		~Path();
+		bool IsMarker() const;
+		bool IsRealtive() const;
+		bool IsAbsoulte() const;
+		bool IsExisting() const;
 
-		void SetMarker(const std::string& marker)
-		{
-			std::string pathStr = m_Path.string() + marker;
+		Origne GetMarkerOrigine() const;
+		Origne GetMarkerExpextedPath() const;
+
+		void SetMarker(Origne origne);
+		void GenertaMarker();
+		void ClearOringenMarker();
+
+
+		inline Path& operator=(const Path& path) {
+			m_Path = path.m_Path;
+			return *this;
 		}
 
+		inline Path& operator=(const std::string& pathStr) {
+			m_Path = pathStr;
+			ConvertUniverselPath();
+			return *this;
+		}
+
+		inline Path& operator=(const std::filesystem::path& pathStr) {
+			m_Path = pathStr;
+			ConvertUniverselPath();
+			return *this;
+		}
+
+		inline Path& operator=(const char* pathCharcter) {
+			m_Path = pathCharcter;
+			ConvertUniverselPath();
+			return *this;
+		}
+
+		inline Path& operator/=(const std::filesystem::path& path) {
+			m_Path /= path;
+			ConvertUniverselPath();
+			return *this;
+		}
+
+		inline Path& operator/=(const std::string& path) {
+			m_Path /= path;
+			ConvertUniverselPath();
+			return *this;
+		}
+
+		inline Path& operator/=(const char* path) {
+			m_Path /= path;
+			return *this;
+		}
 
 	private:
-		
+		void ConvertUniverselPath();
+		// --- private varibles ------------------------------------------------------------------------------------------------------
 		std::filesystem::path m_Path;
+		// --- private frinds -----------------------------------------------------------------------------------------------------
+		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, == , m_Path);
+		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, != , m_Path);
+
+		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, < , m_Path);
+		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, <= , m_Path);
+
+		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, > , m_Path);
+		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, >= , m_Path);
+
+		friend Path operator/(const Path& a, const Path& b);
+		friend Path operator/(const Path& a, const std::filesystem::path& b);
+		friend Path operator/(const Path& a, const std::string& b);
+		friend Path operator/(const Path& a, const char* b);
 	};
+
+
+	RY_NONE_MEBER_OPERATOR_BOOL(Path, == , &&, m_Path);
+	RY_NONE_MEBER_OPERATOR_BOOL(Path, != , &&, m_Path);
+	RY_NONE_MEBER_OPERATOR_BOOL(Path, < , &&, m_Path);
+	RY_NONE_MEBER_OPERATOR_BOOL(Path, <= , &&, m_Path);
+	RY_NONE_MEBER_OPERATOR_BOOL(Path, > , &&, m_Path);
+	RY_NONE_MEBER_OPERATOR_BOOL(Path, >= , &&, m_Path);
+
+	inline Path operator / (const Path& a, const Path& b) {
+		std::filesystem::path path = a.m_Path / b.m_Path;
+		return Path(path);
+	}
+
+	inline Path operator / (const Path& a, const std::filesystem::path& b) {
+		std::filesystem::path path = a.m_Path / b;
+		return Path(path);
+	}
+
+	inline Path operator / (const Path& a, const std::string& b) {
+		std::filesystem::path path = a.m_Path / std::filesystem::path(b);
+		return Path(path);
+	}
+
+	inline Path operator / (const Path& a, const char* b) {
+		std::filesystem::path path = a.m_Path / std::filesystem::path(b);
+		return Path(path);
+	}
+
 }
