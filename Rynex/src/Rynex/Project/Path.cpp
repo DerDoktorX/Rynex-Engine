bb7c94858d6be7a9a3ee97ab94c3bd822a7ccd9e
DerDoktorX <>
2026-08-10 16:15:51 +0200

    Path class

--- Rynex/src/Rynex/Project/Path.cpp
+++ Rynex/src/Rynex/Project/Path.cpp
@@ -3,6 +3,7 @@
 #include <Rynex/Project/Project.h>
 
 namespace Rynex {
+	
 
 	Path::Path(const Path& path)
 		: m_Path(path.m_Path)
@@ -33,7 +34,7 @@ namespace Rynex {
 
 	bool Path::IsMarker() const
 	{
-		return Origne::None != GetMarkerOrigine();
+		return IsPathMarked(m_Path);
 	}
 
 	bool Path::IsRealtive() const
@@ -53,56 +54,27 @@ namespace Rynex {
 
 	Path::Origne Path::GetMarkerOrigine() const
 	{
-		constexpr uint32_t markerCount = 4;
-		constexpr uint32_t markerVaildCount = 2;
-		constexpr const char* markersArray[markerCount] = {
-			RY_PATH_PROJECT_MARKER_STR,
-			RY_PATH_ENGINE_MARKER_STR,
-			RY_PATH_NO_VAILD_MARKER_STR,
-			""
-		};
-
-		uint32_t i = 0;
-		while (i < markerCount && !Project::HasStringInPath(m_Path, markersArray[i]))
-		{
-			i++;
-		}
-		if (markerVaildCount < i)
-		{
-			RY_CORE_ERROR("Diden't found a vaild Marker! {}", i);
-			Origne origne = Origne::None;
-			return origne;
-		}
-		Origne origne = static_cast<Origne>(i);
+		Origne origne = GetPathOrignFromMarkerPath(m_Path);
 		return origne;
 	}
 
-	Path::Origne Path::GetMarkerExpextedPath() const
+	Path::Origne Path::GetExpextedMarkerOrigne() const
 	{
 		Origne origne = GetMarkerOrigine();
-		if (Origne::None != origne) {
+		if (IsOrignePathMarked(origne)) {
 			return origne;
 		}
 		RY_CORE_ASSERT(false, "not implement, marker set!");
-
 		return origne;
 	}
 
 
 	void Path::SetMarker(Origne origne)
 	{
-		if (IsMarker()) {
+		if (IsMarker())
 			ClearOringenMarker();
-		}
-		constexpr const char* markersArray[] = {
-			RY_PATH_NO_VAILD_MARKER_STR
-			RY_PATH_PROJECT_MARKER_STR,
-			RY_PATH_ENGINE_MARKER_STR,
-		};
-		uint32_t index = static_cast<uint32_t>(origne);
 
-		std::string pathStr = m_Path.string() + markersArray[index];
-		m_Path = pathStr;
+		m_Path = GetResolveMarkerPath(origne);
 	}
 
 	void Path::GenertaMarker()
@@ -113,15 +85,177 @@ namespace Rynex {
 	void Path::ClearOringenMarker()
 	{
 		Origne origne = GetMarkerOrigine();
-		if (Origne::None != origne)
+		if (IsOrignePathMarked(origne))
 			return;
 
-		RY_CORE_ASSERT(false, "not implement, marker set!");
+		m_Path = RemovePathMarker(m_Path, origne);
+	}
+
+	std::filesystem::path Path::GetProjectDiretory()
+	{
+		return Project::GetActiveProjectDirectory();
+	}
+
+	std::filesystem::path Path::GetEngineDiretory()
+	{
+		RY_CORE_WARN("Engine Directory is curetly GetWorkingDiretory.");
+		return GetWorkingDiretory();
+	}
+
+	std::filesystem::path Path::GetWorkingDiretory()
+	{
+		std::filesystem::path workingDiretory = std::filesystem::current_path();
+		ConvertUniverselPath(workingDiretory);
+		return workingDiretory;
 	}
 
 	void Path::ConvertUniverselPath()
 	{
-		std::string genaricPathStr = m_Path.generic_string();
-		m_Path = genaricPathStr;
+		ConvertUniverselPath(m_Path);
 	}
+
+	std::filesystem::path Path::GetResolveMarkerPath(Origne origne) const
+	{
+		std::filesystem::path absoluteMarkerPath = GetPathAbsoluteMarker(origne);
+		std::filesystem::path resolvePath = absoluteMarkerPath / m_Path;
+		ConvertUniverselPath(resolvePath);
+		return resolvePath;
+	}
+
+	uint32_t Path::GetMarkerCarkterCount(Origne origne)
+	{
+		int8_t origneIndex = GetOrigneIndex(origne);
+		uint32_t count = GetMarkerLength(PATH_MARKER_STR[origneIndex]);
+		return count;
+	}
+
+	bool Path::IsPathMarked(const std::filesystem::path& markedPath)
+	{
+		Origne origne = GetMarkedPathOrigine(markedPath);
+		return IsOrignePathMarked(origne);
+	}
+
+	bool Path::IsOrignePathMarked(Origne origne)
+	{
+		return Origne::None != origne;
+	}
+
+	std::filesystem::path Path::RemovePathMarker(const std::filesystem::path& path)
+	{
+		Origne origne = GetMarkedPathOrigine(path);
+		return RemovePathMarker(path, origne);
+	}
+
+	std::filesystem::path Path::RemovePathMarker(const std::filesystem::path& markedPath, Origne origne)
+	{
+		uint32_t markerCount = GetMarkerCarkterCount(origne);
+		std::string markerPathStr = markedPath.string();
+		std::string pathStr = markerPathStr.substr(markerCount);
+		std::filesystem::path path = pathStr;
+		return path;
+	}
+
+	std::filesystem::path Path::GetReltivePath(const std::filesystem::path& path)
+	{
+		Origne origne = GetMarkedPathOrigine(path);
+		RY_CORE_ASSERT(IsOrignePathMarked(origne), "file Path not Marked!");
+		std::filesystem::path reltivePath = RemovePathMarker(path, origne);
+		return reltivePath;
+	}
+
+	std::filesystem::path Path::GetReltivePath(const std::filesystem::path& path, const std::filesystem::path& origene)
+	{
+		if (IsPathMarked(path)) {
+			return GetReltivePath(path);
+		}
+		std::filesystem::path reltivePath;
+		if (path.is_relative()) {
+			reltivePath = path;
+			return reltivePath;
+		}
+		reltivePath = std::filesystem::relative(path, origene);
+		return reltivePath;
+	}
+
+	std::filesystem::path Path::GetPathAbsoluteMarker(Origne origne)
+	{
+		switch (origne)
+		{
+		case Path::Origne::None:	return std::filesystem::path("");
+		case Path::Origne::Unknown:	return std::filesystem::path("");
+		case Path::Origne::Engine:	
+		{
+			std::filesystem::path enginePath = GetEngineDiretory();
+			ConvertUniverselPath(enginePath);
+			return enginePath;
+		}
+		case Path::Origne::Project:
+		{
+			std::filesystem::path projectPath = GetProjectDiretory();
+			ConvertUniverselPath(projectPath);
+			return projectPath;
+		}		
+		default:
+			RY_CORE_ASSERT(false, "Not defined Oringe!");
+		}
+		return std::filesystem::path("");
+	}
+
+	Path::Origne Path::GetMarkedPathOrigine(const std::filesystem::path& markedPath)
+	{
+		Origne origne = GetPathOrignFromMarkerPath(markedPath);
+		if (IsOrignePathMarked(origne)) {
+			return origne;
+		}
+		RY_CORE_ASSERT(false, "not implement, marker set!");
+		return Origne::None;
+	}
+
+	Path::Origne Path::GetPathOrignFromMarkerPath(const std::filesystem::path& markedPath)
+	{
+		int8_t i = PATH_MARKER_COUNT - 1;
+		while (0 <= i && !Project::HasStringInPath(markedPath, PATH_MARKER_STR[i]))
+		{
+			i--;
+		}
+		if (0 <= i)
+		{
+			RY_CORE_ERROR("Diden't found a vaild Marker! {}", i);
+			Origne origne = Origne::None;
+			return origne;
+		}
+		Origne origne = static_cast<Origne>(i);
+		return origne;
+	}
+
+	Path::Origne Path::GetExpextedOrigineFromPath(const std::filesystem::path& path)
+	{
+		int8_t i = PATH_MARKER_COUNT - 1;
+		const std::string relativePathSearch[] = {
+			RY_PATH_EXPEXT_ENGINE_RELATIV_START_STR,
+			"",
+		};
+		while (PATH_MARKER_INDEX <= i && !Project::HasStringInPath(path, PATH_MARKER_STR[i]))
+		{
+			i--;
+		}
+		if (PATH_MARKER_INDEX <= i)
+		{
+			RY_CORE_ERROR("Diden't found a vaild Marker! {}", i);
+			Origne origne = Origne::None;
+			return origne;
+		}
+
+		Origne origne = static_cast<Origne>(i);
+		return origne;
+	}
+
+	void Path::ConvertUniverselPath(std::filesystem::path& path)
+	{
+		std::string genaricPathStr = path.generic_string();
+		path = genaricPathStr;
+	}
+
+
+
 }
--- Rynex/src/Rynex/Project/Path.h
+++ Rynex/src/Rynex/Project/Path.h
@@ -1,23 +1,46 @@
 #pragma once
 
+
+#define RY_PATH_PROJECT_MARKER_STR "Project#!#"
+#define RY_PATH_ENGINE_MARKER_STR "Engine#!#"
+#define RY_PATH_NO_VAILD_MARKER_STR "#//#NotVaild#//#"
+#define RY_PATH_EXPEXT_ENGINE_RELATIV_START_STR "../Rynex-Editor/Editor-Assets"
+
+#define RY_PATH_PROJECT_MARKER_WSTR L"Project#!#"
+#define RY_PATH_ENGINE_MARKER_WSTR L"Engine#!#"
+#define RY_PATH_NO_VAILD_MARKER_WSTR L"#//#NotVaildMarker#//#"
+#define RY_PATH_EXPEXT_ENGINE_RELATIV_START_WSTR L"../Rynex-Editor/Editor-Assets"
+
+
 namespace Rynex {
 	class Path
 	{
 	public:
-		enum class Origne
-		{
-			None = 0,
-			Unknown = 1,
-			Engine = 2,
-			Project = 3,
+		inline static constexpr int8_t PATH_MARKER_COUNT = 4; // path marker count
+		inline static constexpr int8_t PATH_MARKER_INDEX = 2; // path marker count
 
+		inline static constexpr const char* const Path::PATH_MARKER_STR[PATH_MARKER_COUNT]={
+			"",								// no marker at all
+			RY_PATH_NO_VAILD_MARKER_STR,	// not vaild marker 
+			RY_PATH_PROJECT_MARKER_STR,		// engine marker 
+			RY_PATH_ENGINE_MARKER_STR		// project marker 
 		};
-		// --- public methodes -------------------------------------------------------------------------------------------------------
+	// --- public enum class --------------------------------------------------------------------------------------------------
+
+		enum class Origne : int8_t
+		{
+			None = 0,		// no marker at all
+			Unknown = 1,	// not vaild marker 
+			Engine = 2,		// engine marker 
+			Project = 3		// project marker 
+		};
+	// --- public methodes ----------------------------------------------------------------------------------------------------
 
 		Path(const Path& file);
 		Path(const std::string& path);
 		Path(const std::filesystem::path& path);
-		Path(const char* path);		~Path();
+		Path(const char* path);		
+		~Path();
 
 		bool IsMarker() const;
 		bool IsRealtive() const;
@@ -25,7 +48,10 @@ namespace Rynex {
 		bool IsExisting() const;
 
 		Origne GetMarkerOrigine() const;
-		Origne GetMarkerExpextedPath() const;
+		Origne GetMarkerOrignePath() const;
+
+		std::filesystem::path GetAbsolutePath() const;
+		std::filesystem::path GetRelativePath(Origne origne = Origne::Engine) const;
 
 		void SetMarker(Origne origne);
 		void GenertaMarker();
@@ -71,12 +97,47 @@ namespace Rynex {
 			m_Path /= path;
 			return *this;
 		}
-
+	// --- public static methodes ---------------------------------------------------------------------------------------------
+		static std::filesystem::path GetProjectDiretory();
+		static std::filesystem::path GetEngineDiretory();
+		static std::filesystem::path GetWorkingDiretory();
 	private:
 		void ConvertUniverselPath();
-		// --- private varibles ------------------------------------------------------------------------------------------------------
+		std::filesystem::path GetResolveMarkerPath(Origne origne) const;
+
+	// --- private static methodes --------------------------------------------------------------------------------------------
+
+		static inline int8_t GetOrigneIndex(Origne origne) { return static_cast<int8_t>(origne); }
+		static uint32_t GetMarkerCarkterCount(Origne origne);
+		static bool IsPathMarked(const std::filesystem::path& markedPath);
+		static bool IsOrignePathMarked(Origne origne) { return Origne::None != origne; } ;
+
+		static std::filesystem::path RemovePathMarker(const std::filesystem::path& markedPath);
+		static std::filesystem::path RemovePathMarker(const std::filesystem::path& markedPath, Origne origne);
+
+		static std::filesystem::path GetReltivePath(const std::filesystem::path& path);
+		static std::filesystem::path GetReltivePath(const std::filesystem::path& path, const std::filesystem::path& origene);
+
+		static std::filesystem::path GetPathAbsoluteMarker(Origne origne);
+		static Origne GetMarkedPathOrigine(const std::filesystem::path& markedPath);
+		static Origne GetMarkerExpextedPathOrigne(const std::filesystem::path& markedPath);
+
+
+		static void ConvertUniverselPath(std::filesystem::path& path);
+	// --- private constexpr static methodes ----------------------------------------------------------------------------------
+		inline static constexpr uint32_t GetMarkerLength(const char* const marker)
+		{
+			uint32_t count = 0;
+			constexpr char stringEnd = 0;
+			while (stringEnd != marker[count])
+			{
+				count++;
+			}
+			return count;
+		}
+	// --- private varibles ---------------------------------------------------------------------------------------------------
 		std::filesystem::path m_Path;
-		// --- private frinds -----------------------------------------------------------------------------------------------------
+	// --- private frinds -----------------------------------------------------------------------------------------------------
 		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, == , m_Path);
 		RY_ADD_NONE_MEBER_OPERATOR_FUNC_AS_FRIND(Path, bool, != , m_Path);
 
